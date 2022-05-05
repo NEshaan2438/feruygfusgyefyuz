3e7c61db3d7770b6f57d7ea5f7d9ca6953fdd5c6
tuvalu1 <87214362+tuvalu1@users.noreply.github.com>
2022-05-04 21:01:21 -0500

    Add files via upload

--- room.js
+++ room.js
@@ -1,3 +1,4 @@
+username = localStorage.getItem("username_letschat");
 var firebaseConfig = {
     apiKey: "AIzaSyDdyrKS8ViQMKUYXp3VnaPDL1Op9FZrXWA",
     authDomain: "letschat-89647.firebaseapp.com",
@@ -12,7 +13,6 @@ firebase.initializeApp(firebaseConfig);
 
 console.log("hi... i guess");
 
-username = localStorage.getItem("username_letschat");
 document.getElementById("welcome").innerHTML = "Hello, " + username  + "!";
 
 function addRoom() {
@@ -20,6 +20,19 @@ function addRoom() {
     localStorage.setItem("room", room);
     window.location = "inroom.html";
 }
+
+function getData() {
+    firebase.database().ref("/").on('value', function(snapshot) {
+    document.getElementById("output").innerHTML ="";
+    snapshot.forEach(function(childSnapshot) {
+    childKey = childSnapshot.key;
+    Room_names = childKey;
+    //stert
+    document.getElementById("rooms").innerHTML += "<h3 class='roomNameThing' id=" + Room_names + " onclick='redirect(this.id)'>" + Room_names + "</h3> <hr>";
+    //eenb
+});});}
+
+getData();
 
 function redirect(room_name) {
     roomname = room_name;
